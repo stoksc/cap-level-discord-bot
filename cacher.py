709337c35690354592a7b9924f03875360f6cf11
stoksc <bradley.laney@gmail.com>
2017-11-03 19:01:18 -0400

    added query result caching for frequent queries.

---
+++ cacher.py
@@ -0,0 +1,20 @@
+import pickle
+import os
+import glob
+import data
+
+def cache_embed(discord_em, query_type, query_str):
+    query_dir = '\\'.join((os.getcwd(), query_type))
+    query_file = query_dir + '\\' + query_str + '.txt'
+    with open(query_file, 'wb') as f:
+        pickle.dump(discord_em, f)
+    if sum([os.stat(query_file).st_size for f in os.listdir(query_dir)]) > data.MAX_SIZE:
+        lru_file = min([(os.stat(query_file).st_atime, f) for f in os.listdir(query_dir)])
+        os.remove(query_dir + '\\' + lru_file[1])
+
+def uncache_embed(query_type, query_str):
+    try:
+        with open('\\'.join((os.getcwd(), query_type, query_str + '.txt')), 'rb') as f:
+            return pickle.load(f)
+    except FileNotFoundError:
+        return None
